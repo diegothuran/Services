fc5c4b633a68c421ac49e7a3d208b286db0e8b56
diego <day23any>
2017-01-23 16:09:14 -0300

    Login e Logout adicionados. Correção de bugs

--- Services/settings.py
+++ Services/settings.py
@@ -79,7 +79,7 @@ WSGI_APPLICATION = 'Services.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
-'''
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -100,7 +100,7 @@ DATABASES = {
         'TEST_COLLATION': "utf8_general_ci",
     }
 }
-
+'''
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
 
